db18384e1876e8129449cbee59498713d3fae827
Alden Greene <46254363+aldeng323@users.noreply.github.com>
2019-06-29 13:13:24 -0700

    mods
    updated dates

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("May 31, 2019 10:00:00").getTime();
+var countDownDate = new Date("Aug 31, 2019 10:00:00").getTime();
 
 var countDownfunction = setInterval(function() {
 
